625710b12c173bb53ca34cade5cf536379c4fbb4
Rachel Whitmore <rachel.whitmore@mandg.com>
2021-03-28 22:07:40 +0100

    added new code

--- RomanNumeralKata.Source/RomanNumeral.cs
+++ RomanNumeralKata.Source/RomanNumeral.cs
@@ -7,18 +7,57 @@ namespace RomanNumeralKata.Source
     {
         public string ToRoman(int arabic)
         {
-            var arabicToRoman = new Dictionary<int,string>
-            {
-                {1,"I"},
-            };
+
             var roman = string.Empty;
+            var arabicToRoman = new Dictionary<int, string>
+            {
+                {1, "I"},
+                {5, "V"},
+                {10, "X"},
+                {50, "L"},
+                {100, "C"},
+                {500, "D"},
+                {1000, "M"},
+            };
+
+            if (ArabicToRomanContainsKey(arabic, arabicToRoman))
+            {
+                roman += arabicToRoman[arabic];
+                return roman;
+            }
+
             var timesGoingRound = 1;
-            while (timesGoingRound <=  arabic)
+            if (arabic <= 3)
+            {
+                return SingleNumeral(arabic, timesGoingRound, roman, arabicToRoman);
+            }
+
+            var closestMultiple = 5;
+            var extraNumberToAdd = arabic % closestMultiple;
+            if (ArabicToRomanContainsKey(closestMultiple, arabicToRoman))
+            {
+                roman += arabicToRoman[closestMultiple];
+                return SingleNumeral(extraNumberToAdd, timesGoingRound, roman, arabicToRoman);
+            }
+            
+            return roman;
+
+        }
+
+        private static string SingleNumeral(int arabic, int timesGoingRound, string roman, Dictionary<int, string> arabicToRoman)
+        {
+            while (timesGoingRound <= arabic)
             {
                 roman += arabicToRoman[1];
                 timesGoingRound++;
             }
+
             return roman;
+        }
+
+        private static bool ArabicToRomanContainsKey(int arabic, Dictionary<int, string> arabicToRoman)
+        {
+            return arabicToRoman.ContainsKey(arabic);
         }
     }
 }
--- RomanNumerlKata.Tests/UnitTest1.cs
+++ RomanNumerlKata.Tests/UnitTest1.cs
@@ -8,6 +8,13 @@ namespace RomanNumerlKata.Tests
         [TestCase(1,"I")]
         [TestCase(2,"II")]
         [TestCase(3,"III")]
+        [TestCase(5,"V")]
+        [TestCase(10,"X")]
+        [TestCase(500,"D")]
+        [TestCase(6,"VI")]
+        [TestCase(7,"VII")]
+        [TestCase(8,"VIII")]
+        [TestCase(11,"XI")]
         public void ReturnRoman_GivenArabic(int arabic, string expected)
         {
             var romanNumeral = new RomanNumeral();
